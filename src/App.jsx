f2ab88a2509aaba24b62d700df594c41daa9084c
hafidhaVoideu <hafidha@bouabdelli.com>
2023-10-29 01:16:16 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { setGlowEffectRx } from "./utils/functions";
 import Homepage from "./scenes/homepage/Homepage";
 import { Routes, Route } from "react-router-dom";
@@ -11,9 +11,19 @@ import FeaturedProj from "./scenes/dashboard/pages/featuredProjects/FeaturedProj
 import PendingSynergies from "./scenes/dashboard/pages/pendingSynergies/PendingSynergies";
 import ProjectToRender from "./scenes/dashboard/pages/projects/project/ProjectToRender";
 import SynergyToRender from "./scenes/dashboard/pages/synergies/SynergyToRender";
+
+import axios from "axios";
+
+const access_token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImRubEJvdCIsImlhdCI6MTY5NDU0NTE0M30.-1kktNej16aURKwdXa1K-4-zwC9b_t0EkAmEewJFF5c";
+
+axios.defaults.headers.common["Authorization"] = `Bearer ${access_token}`;
+
 function App() {
   const location = useLocation();
   const { setTab } = useGlobalContextUser();
+
+  const [data, setData] = useState();
   useEffect(() => {
     setGlowEffectRx();
   });
@@ -24,9 +34,28 @@ function App() {
     if (activeTab) setTab(activeTab.name);
   }, [location.pathname]);
 
+  const config = {
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers":
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+      "Access-Control-Request-Method": "GET, POST, DELETE, PUT, OPTIONS",
+    },
+  };
+
+  useEffect(() => {
+    axios
+      .get("https://68.183.108.138:3000/api/projects/", {}, config)
+      .then((response) => console.log("response:", response.data))
+      .catch((error) => console.log("error", error.response?.status));
+  }, []);
+
   return (
     <>
-      <Routes>
+      <div></div>
+
+      {/* <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/dashboard" element={<DashboardLayout />}>
           <Route index element={<FeaturedProj />} />
@@ -35,10 +64,10 @@ function App() {
           <Route path="projects" element={<ProjectToRender />} />
           <Route path="synergies" element={<SynergyToRender />} />
           <Route path="pendingsynergies" element={<PendingSynergies />} />
-        </Route>
+        </Route> */}
 
-        {/* <Route path="/*" element={<NotFound />}></Route> */}
-      </Routes>
+      {/* <Route path="/*" element={<NotFound />}></Route> */}
+      {/* </Routes> */}
     </>
   );
 }
---
+++ src/axios/axios.tsx
@@ -0,0 +1,10 @@
+// import axios from "axios";
+
+// const instance = axios.create({
+//   baseURL: 'https://jsonplaceholder.typicode.com',
+//    headers: {
+//     common: {
+//       Authorization: 'AUTH_TOKEN_FROM_INSTANCE'
+//     }}
+// });
+// axios.defaults.headers.common["Authorization"] = `Bearer ${access_token}`;
